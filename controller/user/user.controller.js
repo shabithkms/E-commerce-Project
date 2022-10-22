e4a82958dc8b8198366df8005e12650c67bde44d
shabithkms <shabithkms2035@gmail.com>
2022-10-22 15:23:10 +0530

    user home

---
+++ controller/user/user.controller.js
@@ -0,0 +1,7 @@
+const homePage = async (req, res) => {
+    return res.render('user/index')
+}
+
+module.exports = {
+    homePage,
+}
--- routes/user/user.routes.js
+++ routes/user/user.routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 require('express-group-routes')
 
+const userController = require('../../controller/user/user.controller')
+
+router.get('/',userController.homePage)
+
 
 
 
