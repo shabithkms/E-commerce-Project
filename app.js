e5d71229cedb81e7c1a56f87f6553e7a753fd405
shabithkms <shabithkms2035@gmail.com>
2022-10-22 19:22:03 +0530

    added public folders in vercel.json

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static('public'))
-
+console.log(process.env);
 //session
 app.use(
     session({
